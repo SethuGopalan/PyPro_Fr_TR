11b4f05422eb58c036342fff816b7d1eaa502cfc
Sethu Gopalan <sethugopalan@outlook.com>
2023-11-10 12:53:34 -0500

    update terraform data source

--- myenv/config.tf
+++ myenv/config.tf
@@ -4,44 +4,46 @@
 
 resource "aws_default_vpc" "default" {
 
+
+
 }
 
-resource "aws_security_group" "Eks_dashapp_Dock_sg" {
+# resource "aws_security_group" "Eks_dashapp_Dock_sg" {
 
-  name = "Eks_dashapp_dock_sg"
+#   name = "Eks_dashapp_dock_sg"
 
-  vpc_id = aws_default_vpc.default.id
-  # where you wnat to allow trafic from
-  # http
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+#   vpc_id = aws_default_vpc.default.id
+#   # where you wnat to allow trafic from
+#   # http
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
 
-  }
-  # ssh
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   # ssh
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
 
-  }
+#   }
 
 
 
-  egress {
+#   egress {
 
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = -1
+#     cidr_blocks = ["0.0.0.0/0"]
 
-  }
+#   }
 
-  tags = {
-    name = "Eks_dashapp_Dock_sg"
-  }
 
-}
+#  tags = {
+#     name = "Eks_dashapp_Dock_sg"
+#   }
+# }
--- myenv/data_providers.tf
+++ myenv/data_providers.tf
@@ -1,6 +1,11 @@
 
-data "aws_security_group" "Eks_dashapp_Dock_sg" {
+data "aws_security_group" "Eks_dashapp_dock_sg" {
   name = "Eks_dashapp_dock_sg"
+
+  tags = {
+    name = "Eks_dashapp_dock_sg"
+  }
+
 }
 
 data "aws_iam_role" "eks_dashapp_role" {
