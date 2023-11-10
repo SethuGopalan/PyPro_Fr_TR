2dc92b537fb5a3bfcbfb54260d3a51cb6d9c80e3
Sethu Gopalan <sethugopalan@outlook.com>
2023-11-10 13:46:53 -0500

    update terraform data source

--- myenv/data_providers.tf
+++ myenv/data_providers.tf
@@ -1,35 +1,35 @@
 
-# data "aws_security_group" "Eks_dashapp_dock_sg" {
-#   # name = "Eks_dashapp_dock_sg"
+data "aws_security_group" "Eks_dashapp_dock_sg" {
+  # name = "Eks_dashapp_dock_sg"
 
-#   # tags = {
-#   #   name = "Eks_dashapp_dock_sg"
-#   # }
+  # tags = {
+  #   name = "Eks_dashapp_dock_sg"
+  # }
 
+}
+
+# data "aws_iam_role" "eks_dashapp_role" {
+#   name = "eks_dashapp_role"
 # }
 
-data "aws_iam_role" "eks_dashapp_role" {
-  name = "eks_dashapp_role"
-}
-
-data "aws_iam_policy" "AmazonEKSClusterFullAccess" {
-  name = "AmazonEKSClusterFullAccess"
-}
-
-# data "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
-#   role       = data.aws_iam_role.eks_dashapp_role.arn
-#   policy_arn = data.aws_iam_policy.AmazonEKSClusterFullAccess.arn
+# data "aws_iam_policy" "AmazonEKSClusterFullAccess" {
+#   name = "AmazonEKSClusterFullAccess"
 # }
 
+# # data "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
+# #   role       = data.aws_iam_role.eks_dashapp_role.arn
+# #   policy_arn = data.aws_iam_policy.AmazonEKSClusterFullAccess.arn
+# # }
 
-data "aws_subnets" "default_subnets" {
 
-  filter {
+# data "aws_subnets" "default_subnets" {
 
-    #   vpc_id = aws_default_vpc.default.id
-    name = "vpc-id"
+#   filter {
 
-    values = [aws_default_vpc.default.id]
-  }
+#     #   vpc_id = aws_default_vpc.default.id
+#     name = "vpc-id"
 
-}
+#     values = [aws_default_vpc.default.id]
+#   }
+
+# }
--- myenv/resources.tf
+++ myenv/resources.tf
@@ -1,20 +1,31 @@
 resource "aws_eks_cluster" "my-eks-dashapp-cluster" {
   name = "my-eks-dashapp-cluster"
 
-  role_arn = data.aws_iam_role.eks_dashapp_role.arn
+  role_arn = aws_iam_role.eks_dashapp_role.arn
 
   vpc_config {
-    security_group_ids = [aws_security_group.Eks_dashapp_dock_sg.id]
-    subnet_ids         = [data.aws_subnets.default_subnets.id]
+    security_group_ids = [data.aws_security_group.Eks_dashapp_dock_sg.id]
+    subnet_ids         = [aws_subnets.default_subnets.id]
   }
 
 
 }
+resource "aws_subnets" "default_subnets" {
 
-resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
-  role       = data.aws_iam_role.eks_dashapp_role.arn
-  policy_arn = data.aws_iam_policy.AmazonEKSClusterFullAccess.arn
+  filter {
+
+    #   vpc_id = aws_default_vpc.default.id
+    name = "vpc-id"
+
+    values = [aws_default_vpc.default.id]
+  }
+
 }
+
+# resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
+#   role       = data.aws_iam_role.eks_dashapp_role.arn
+#   policy_arn = data.aws_iam_policy.AmazonEKSClusterFullAccess.arn
+# }
 # resource "aws_security_group_rule" "ingress_http" {
 
 #   type              = "ingress"
@@ -43,48 +54,48 @@ resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
 #   security_group_id = data.aws_security_group.Eks_dashapp_dock_sg.id
 # }
 
-# resource "aws_iam_role" "eks_dashapp_role" {
-#   name               = "eks_dashapp_role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "eks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
-# resource "aws_iam_policy" "AmazonEKSClusterFullAccess" {
-#   name   = "AmazonEKSClusterFullAccess"
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "eks:*",
-#         "ec2:*",
-#         "iam:*",
-#         "s3:*",
-#         "logs:*",
-#         "cloudwatch:*"
-#       ],
-#       "Resource": "*"
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "eks_dashapp_role" {
+  name               = "eks_dashapp_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy" "AmazonEKSClusterFullAccess" {
+  name   = "AmazonEKSClusterFullAccess"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "eks:*",
+        "ec2:*",
+        "iam:*",
+        "s3:*",
+        "logs:*",
+        "cloudwatch:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
-#   role       = aws_iam_role.eks_dashapp_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterFullAccess"
-# }
+resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
+  role       = aws_iam_role.eks_dashapp_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterFullAccess"
+}
 
