2814722bea21e30df3c0c1e1475f2f23a6bc669b
Sethu Gopalan <sethugopalan@outlook.com>
2023-11-10 23:40:15 -0500

    update terraform data source

--- myenv/data_providers.tf
+++ myenv/data_providers.tf
@@ -27,7 +27,7 @@ data "aws_subnets" "default_subnets" {
   filter {
 
     #   vpc_id = aws_default_vpc.default.id
-    name = "vpc-id"
+    name = "vpc_id"
 
     values = [aws_default_vpc.default.id]
   }
