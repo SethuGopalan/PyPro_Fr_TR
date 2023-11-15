b4c7b653e58e32ae13377c7300a684c9fef38096
Sethu Gopalan <sethugopalan@outlook.com>
2023-11-15 16:43:12 -0500

    update s3 permission

--- myenv/backend.tf
+++ myenv/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-dashapp-bucket-001"
+    bucket = "octopusmsiinstaller"
     key    = "Kub-dashApp001-dev.tfstate"
     region = "us-east-1"
     # Include additional backend configurations if needed
--- myenv/resources.tf
+++ myenv/resources.tf
@@ -88,6 +88,19 @@ resource "aws_iam_policy" "eks_custom_cluster_access" {
         "cloudwatch:*"
       ],
       "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::octopusmsiinstaller",
+        "arn:aws:s3:::octopusmsiinstaller/*"
+      ]
     }
   ]
 }
@@ -101,41 +114,41 @@ resource "aws_iam_role_policy_attachment" "eks_dashapp_policy" {
   policy_arn = aws_iam_policy.eks_custom_cluster_access.arn
 }
 
-resource "aws_s3_bucket" "dashapp_bucket" {
-  bucket = "eks-dashapp-bucket-001"
+# resource "aws_s3_bucket" "dashapp_bucket" {
+#   bucket = "eks-dashapp-bucket-001"
 
-}
+# }
 
-resource "aws_iam_policy" "s3_bucket_access" {
-  name        = "S3BucketAccessPolicy"
-  description = "Policy to access specific S3 bucket"
+# resource "aws_iam_policy" "s3_bucket_access" {
+#   name        = "S3BucketAccessPolicy"
+#   description = "Policy to access specific S3 bucket"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ],
-        Effect = "Allow",
-        Resource = [
-          "${aws_s3_bucket.dashapp_bucket.arn}",
-          "${aws_s3_bucket.dashapp_bucket.arn}/*"
-        ]
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject",
+#           "s3:ListBucket"
+#         ],
+#         Effect = "Allow",
+#         Resource = [
+#           "${aws_s3_bucket.dashapp_bucket.arn}",
+#           "${aws_s3_bucket.dashapp_bucket.arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_user" "dashapp_bucket_user" {
-  name = "dashapp_bucket_user"
-}
+# resource "aws_iam_user" "dashapp_bucket_user" {
+#   name = "dashapp_bucket_user"
+# }
 
-resource "aws_iam_user_policy_attachment" "example_attach" {
-  user       = aws_iam_user.dashapp_bucket_user.name
-  policy_arn = aws_iam_policy.s3_bucket_access.arn
-}
+# resource "aws_iam_user_policy_attachment" "example_attach" {
+#   user       = aws_iam_user.dashapp_bucket_user.name
+#   policy_arn = aws_iam_policy.s3_bucket_access.arn
+# }
 
